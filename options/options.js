f4acc1e46adf06378ffdfa2b0c2f97d01d136f08
Crowdbotics <team@crowdbotics.com>
2022-07-25 09:44:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tik_tok_36401",
+  url: "https://tik_tok_36401.botics.co",
+  api: "https://tik_tok_36401.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
